fbe82d9b54a5c8b7d75a6a34d9eec46a931ef6be
Anurag7972 <anuragsinghsingh589@gmail.com>
2026-08-28 22:36:17 +0530

    solve: array insertion implementation

---
+++ array_insertion.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10] = {7, 8, 9, 1, 5};
+    int n = 5;
+    int index = 2;
+    int x = 5;
+
+    if (index < 0 || index > n) {
+        cout << "Invalid index";
+        return 0;
+    }
+       // shifting element to the right
+    for (int i = n; i > index; i--) {
+        arr[i] = arr[i - 1];
+    }
+     // insert the element
+    arr[index] = x;
+    n++;
+           // displying the array
+    cout << "Array after insertion: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
