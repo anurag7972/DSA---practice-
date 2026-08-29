efea936fdec885dc9cf8ecc4e680cf34bee74933
Anurag7972 <anuragsinghsingh589@gmail.com>
2026-08-29 23:27:28 +0530

    solve minimum  element in array using vector

---
+++ min_element.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of array:";
+    cin>>n;
+    vector<int> arr(n);
+    
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+     int min=arr[0];
+
+     for(int i=0;i<n;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    cout<<"the minimum value  in array  :"<<min;
+}
