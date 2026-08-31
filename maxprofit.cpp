c1f0ab50293c4af391c10cc72fbeaa73d3f046aa
Anurag7972 <anuragsinghsingh589@gmail.com>
2026-08-31 23:40:44 +0530

    find the maximum profit  of ticket

---
+++ maxprofit.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0];
+        int sell = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            }
+
+            int profit = prices[i] - buy;
+
+            if (profit > sell) {
+                sell = profit;
+            }
+        }
+
+        return sell;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    cout << "Maximum Profit: " << solution.maxProfit(prices) << endl;
+
+    return 0;
+}
